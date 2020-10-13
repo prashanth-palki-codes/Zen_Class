e660874d8121dc5cb614d0c159f806e7e68eecf9
Prashanth Palki <prashanthpalki1999@gmail.com>
2020-10-13 14:20:43 +0530

    completed day3 task

---
+++ Day3/script_latlong.js
@@ -0,0 +1,7 @@
+var request=new XMLHttpRequest();
+request.open('GET','https://api.openweathermap.org/data/2.5/weather?lat=20&lon=77&appid=65803ead8f1fa7a51f1a404137052b41',true);
+request.send();
+request.onload=function(){
+	var data=JSON.parse(this.response);
+	console.log(data);
+};
